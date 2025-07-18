9ea0b6bdaab66fb0a7ca9c639b4ce8c875d47f51
Pichau <Ribeirodacostadaniel57@gmail.com>
2025-07-18 13:26:00 -0300

    Ja volto Modal Update

--- src/components/modalProfile.jsx
+++ src/components/modalProfile.jsx
@@ -1,11 +1,25 @@
-import style from "./modalProfile.module.css"
+import style from "./modalProfile.module.css";
+
+import manPet from "../assets/images/manPet.png"
+import man from "../assets/images/man.png"
+
 export default function ModalProfile({ onClose }) {
   return (
-    <>
-    <div className={style.container}>
-        <h1>OLA</h1>
-        <button onClick={onClose}>❌</button> 
+    <div className={style.modalOverlay}>
+      <div className={style.modalContainer}>
+        <button className={style.closeButton} onClick={onClose}>❌</button>
+        <div className={style.modalContainerDivs}>
+                    <div className={style.section}>
+          <img src={manPet} alt="" className={style.sectionImg}/>
+          <button className={style.sectionButton}>ALTERAR INFORMAÇÕES DO PET</button>
+        </div>
+        <div className={style.riscoMeio}></div>
+        <div className={style.section}>
+          <img src={man} alt="" className={style.sectionImg}/>
+          <button className={style.sectionButton}>ALTERAR INFORMAÇÕES DO USUÁRIO</button>
+        </div>
+        </div>
+      </div>
     </div>
-    </>
   );
 }
