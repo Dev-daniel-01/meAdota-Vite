c26b8a05dfbcb89503ee230d155763cb3d1a360e
dev-Daniel-01 <ribeirodacostadaniel57@gmail.com>
2025-07-25 16:40:32 -0300

    corrigindo o footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,6 +4,15 @@ import instagram from "../assets/images/instagram.png";
 import whatsapp from "../assets/images/whatsapp.png";
 
 export const Footer = () => {
+  
+  const acessarInsta = () => {
+    const URLinsta = `https://www.instagram.com/me_adota2025/?next=%2F`
+
+    window.open(URLinsta, "_blank")
+  }
+  
+  const whatsappNumber = "5541999999901";
+  
   return (
     <footer className={style.Footer}>
       
@@ -26,20 +35,8 @@ export const Footer = () => {
       {/* Ícones lado direito */}
       <div className={style.footerSide}>
         <div className={style.footerLinkContainer}>
-          <a className={style.footerLink} href="/">
-            <img 
-              src={instagram} 
-              alt="Instagram" 
-              className={style.iconsFooter} 
-            />
-          </a>
-          <a className={style.footerLink} href="/">
-            <img 
-              src={whatsapp} 
-              alt="Whatsapp" 
-              className={style.iconsFooter} 
-            />
-          </a>
+          <a target="_blank" className={style.footerLink} style={{ cursor: "pointer" }} onClick={acessarInsta}><img src={instagram} alt="Instagram" className={style.iconsFooter} /></a>
+          <a target="_blank" className={style.footerLink} style={{ cursor: "pointer" }} href={`https://wa.me/${whatsappNumber}`}><img src={whatsapp} alt="Whatsapp" className={style.iconsFooter}/></a>
         </div>
       </div>
     </footer>
